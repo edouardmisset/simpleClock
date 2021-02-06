5a5170aa0af34c1ab5a67a6bb5167c35dc1f395e
Edouard Misset <edouardmisset@gmail.com>
2021-02-06 11:34:58 +0100

    Working prototype. Performance improvements needed

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const digitalClock = document.getElementById("digital-clock"),
   hours = document.getElementById("hours"),
-  minutes = document.getElementById("minutes");
+  minutes = document.getElementById("minutes"),
+  seconds = document.getElementById("seconds");
 
 // To display the time when the script is first read
-getTime();
+setTime();
 
 // Function to return the current time in the header
-function getTime() {
-  digitalClock.textContent = new Date().toLocaleTimeString();
+function setTime() {
+  const date = new Date();
+  digitalClock.textContent = date.toLocaleTimeString();
+  hours.style.transform = `rotate(${date.getHours() * 30}deg)`;
+  minutes.style.transform = `rotate(${date.getMinutes() * 6}deg)`;
+  seconds.style.transform = `rotate(${date.getSeconds() * 6}deg)`;
 }
 
 // Repeat this function every 1000  milliseconds
-setInterval(getTime, 1000);
+setInterval(setTime, 1000);
